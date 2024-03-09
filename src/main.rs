26e9c430baad727253592c887ebda3dd9c1ce53d
Connor Ashton <cjconnorashton@gmail.com>
2024-03-09 16:27:33 -0700

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,65 @@ use std::env;
 // Available if you need it!
 // use serde_bencode
 
+fn parse_ben_string(encoded_value: &str) -> serde_json::Value {
+    let colon_index = encoded_value.find(':').unwrap();
+    let number_string = &encoded_value[..colon_index];
+    let number = number_string.parse::<i64>().unwrap();
+    let string =
+        &encoded_value[colon_index + 1..colon_index + 1 + usize::try_from(number).unwrap()];
+    serde_json::Value::String(string.to_string())
+}
+
+fn parse_ben_int(encoded_value: &str) -> serde_json::Value {
+    let num_str = &encoded_value[1..encoded_value.find('e').unwrap()];
+    let num = num_str.parse::<i64>().unwrap();
+    serde_json::json!(num)
+}
+
+fn parse_ben_list(encoded_value: &str) -> serde_json::Value {
+    let mut iter = encoded_value.chars().peekable();
+    let mut items: Vec<serde_json::Value> = vec![];
+
+    while let Some(c) = iter.next() {
+        // String
+        if c.is_ascii_digit() && iter.peek().unwrap() == &':' {
+            // get length of word
+            let length = c.to_digit(10).unwrap();
+
+            // skip colon
+            iter.next();
+
+            // build string
+            let mut res = String::new();
+            for _ in 0..length {
+                let new_char = iter.next().unwrap();
+                res.push(new_char);
+            }
+            items.push(serde_json::json!(res));
+
+        // Integer
+        } else if c == 'i' && iter.peek().unwrap().is_ascii_digit() {
+            let mut res = String::new();
+            let mut cur_char = iter.next().unwrap();
+            while cur_char != 'e' {
+                res.push(cur_char);
+                cur_char = iter.next().unwrap();
+            }
+            items.push(serde_json::json!(res));
+        }
+    }
+
+    serde_json::json!(items)
+}
+
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
     if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string =
-            &encoded_value[colon_index + 1..colon_index + 1 + usize::try_from(number).unwrap()];
-        serde_json::Value::String(string.to_string())
+        parse_ben_string(encoded_value)
     } else if encoded_value.starts_with('i') && encoded_value.ends_with('e') {
-        let num_str = &encoded_value[1..encoded_value.find('e').unwrap()];
-        let num = num_str.parse::<i64>().unwrap();
-        serde_json::json!(num)
+        parse_ben_int(encoded_value)
+    } else if encoded_value.starts_with('l') && encoded_value.ends_with('e') {
+        parse_ben_list(encoded_value)
     } else {
         panic!("Unhandled encoded value: {encoded_value}")
     }
