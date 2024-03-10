b988a7df8a3a562179b05ebb640f35beeb5b275e
Connor Ashton <cjconnorashton@gmail.com>
2024-03-09 17:21:49 -0700

    pass phase 4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, iter::Peekable, str::Chars};
+use std::{any::Any, collections::HashMap, env, iter::Peekable, str::Chars};
 
 // Available if you need it!
 // use serde_bencode
@@ -61,6 +61,47 @@ fn parse_ben_list(iter: &mut Peekable<Chars<'_>>) -> serde_json::Value {
     serde_json::json!(items)
 }
 
+fn parse_ben_dict(iter: &mut Peekable<Chars<'_>>) -> serde_json::Value {
+    let mut is_key = true;
+    let mut last_key = String::new();
+    let mut map: HashMap<String, serde_json::Value> = HashMap::new();
+    let mut val = String::new();
+
+    iter.next();
+
+    while let Some(char) = iter.next() {
+        let mut tmp_val = String::new();
+        if char.is_ascii_digit() && iter.peek().unwrap() == &':' {
+            let length = char.to_digit(10).unwrap();
+            iter.next();
+            for _ in 0..length {
+                let new_char = iter.next().unwrap();
+                tmp_val.push(new_char);
+            }
+            val = tmp_val;
+        } else if char == 'i' && iter.peek().unwrap().is_ascii_digit() {
+            let mut res = String::new();
+            let mut cur_char = iter.next().unwrap();
+            while cur_char != 'e' {
+                res.push(cur_char);
+                cur_char = iter.next().unwrap();
+            }
+            val = res;
+        } else {
+        }
+
+        if is_key {
+            last_key = val.clone();
+            is_key = false;
+        } else {
+            map.insert(String::from(&last_key), serde_json::json!(val));
+            is_key = true;
+        }
+    }
+
+    serde_json::json!(map)
+}
+
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     let mut iter = encoded_value.chars().peekable();
@@ -71,6 +112,8 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     } else if encoded_value.starts_with('l') && encoded_value.ends_with('e') {
         iter.next();
         parse_ben_list(&mut iter)
+    } else if encoded_value.starts_with('d') && encoded_value.ends_with('e') {
+        parse_ben_dict(&mut iter)
     } else {
         panic!("Unhandled encoded value: {encoded_value}")
     }
