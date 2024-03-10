06c08cfaaf594d907ff4e1030330c4750535e850
Connor Ashton <cjconnorashton@gmail.com>
2024-03-09 19:06:36 -0700

    pass 5th stage

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,60 @@
-use std::{collections::HashMap, env, fs, iter::Peekable, str::Chars};
+use std::{collections::HashMap, env, fs, iter::Peekable};
 
-fn parse_ben_string<'a>(iter: &mut Peekable<Chars<'a>>) -> String {
-    let mut length_str = String::new();
-    let mut char = iter.next().unwrap();
-    while char != ':' {
-        length_str.push(char);
-        char = iter.next().unwrap();
+fn parse_ben_string<'a>(iter: &mut Peekable<std::slice::Iter<'a, u8>>) -> String {
+    let mut length_str = Vec::new();
+    loop {
+        let char = iter.next().unwrap();
+        if *char == b':' {
+            break;
+        }
+        length_str.push(*char);
     }
-
-    let length = length_str.parse::<usize>().unwrap();
+    let length = String::from_utf8(length_str)
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
     let mut string = String::with_capacity(length);
-
     for _ in 0..length {
-        string.push(iter.next().unwrap());
+        string.push(*iter.next().unwrap() as char);
     }
-
     string
 }
 
-fn parse_ben_int<'a>(iter: &mut Peekable<Chars<'a>>) -> serde_json::Value {
+fn parse_ben_int<'a>(iter: &mut Peekable<std::slice::Iter<'a, u8>>) -> serde_json::Value {
     iter.next(); // Skip the 'i'
-    let mut num_str = String::new();
-    let mut char = iter.next().unwrap();
-    while char != 'e' {
-        num_str.push(char);
-        char = iter.next().unwrap();
+    let mut num_str = Vec::new();
+    loop {
+        let char = iter.next().unwrap();
+        if *char == b'e' {
+            break;
+        }
+        num_str.push(*char);
     }
-    let num = num_str.parse::<i64>().unwrap();
+    let num = String::from_utf8(num_str).unwrap().parse::<i64>().unwrap();
     serde_json::Value::Number(serde_json::Number::from(num))
 }
 
-fn parse_ben_list<'a>(iter: &mut Peekable<Chars<'a>>) -> serde_json::Value {
+fn parse_ben_list<'a>(iter: &mut Peekable<std::slice::Iter<'a, u8>>) -> serde_json::Value {
     iter.next(); // Skip the 'l'
     let mut items = Vec::new();
-
     loop {
         match iter.peek() {
-            Some('e') => {
+            Some(&b'e') => {
                 iter.next(); // Consume the 'e'
                 break;
             }
             _ => items.push(decode_bencoded_value(iter)),
         }
     }
-
     serde_json::Value::Array(items)
 }
 
-fn parse_ben_dict<'a>(iter: &mut Peekable<Chars<'a>>) -> serde_json::Value {
+fn parse_ben_dict<'a>(iter: &mut Peekable<std::slice::Iter<'a, u8>>) -> serde_json::Value {
     iter.next(); // Skip the 'd'
     let mut map = HashMap::new();
-
     loop {
         match iter.peek() {
-            Some('e') => {
+            Some(&b'e') => {
                 iter.next(); // Consume the 'e'
                 break;
             }
@@ -65,20 +66,19 @@ fn parse_ben_dict<'a>(iter: &mut Peekable<Chars<'a>>) -> serde_json::Value {
             None => panic!("Invalid dictionary format"),
         }
     }
-
     serde_json::json!(map)
 }
 
-fn decode_bencoded_value<'a>(iter: &mut Peekable<Chars<'a>>) -> serde_json::Value {
+fn decode_bencoded_value<'a>(iter: &mut Peekable<std::slice::Iter<'a, u8>>) -> serde_json::Value {
     let mut iter_clone = iter.clone();
     match iter_clone.peek() {
-        Some(c) if c.is_ascii_digit() => {
+        Some(&byte) if byte.is_ascii_digit() => {
             let string = parse_ben_string(iter);
             serde_json::Value::String(string)
         }
-        Some('i') => parse_ben_int(iter),
-        Some('l') => parse_ben_list(iter),
-        Some('d') => parse_ben_dict(iter),
+        Some(&b'i') => parse_ben_int(iter),
+        Some(&b'l') => parse_ben_list(iter),
+        Some(&b'd') => parse_ben_dict(iter),
         _ => panic!("Invalid format"),
     }
 }
@@ -88,21 +88,16 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        let encoded_value = &args[2];
-        let mut iter = encoded_value.chars().peekable();
+        let encoded_value = &args[2].clone().into_bytes();
+        let mut iter = encoded_value.iter().peekable();
         let decoded_value = decode_bencoded_value(&mut iter);
         println!("{decoded_value}");
     } else if command == "info" {
         let file_name = &args[2];
         let bytes = fs::read(file_name).unwrap();
-        let s = String::from_utf8_lossy(&bytes);
-        let mut iter = s.chars().peekable();
+        let mut iter = bytes.iter().peekable();
         let decoded_value = decode_bencoded_value(&mut iter);
-
-        println!(
-            "Tracker URL: {}\nLength: {}",
-            decoded_value["announce"], decoded_value["info"]["length"]
-        );
+        println!("{decoded_value}");
     } else {
         println!("unknown command: {}", args[1]);
     }
